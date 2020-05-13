fa8888b0521ac79a39c79d5ec64fadbc3bb5aac2
Hanjun Goo <goohanjun@gmail.com>
2020-05-13 18:17:37 +0900

    debug msg removed

--- model.py
+++ model.py
@@ -45,24 +45,24 @@ class ApproxEMD(nn.Module):
         q_2 = torch.cat([torch.max(seq_2, dim=1)[0], torch.min(seq_2, dim=1)[0], torch.sum(seq_2, dim=1) / div_seq_len_2.view(-1, 1)], dim=-1)
         q_2 = self.query_act(self.query_layer(q_2))  # [batch, n_hidden]
 
-        print("model_4")
+        # print("model_4")
         v_1 = self.att_read(q_2, seq_1, seq_len_1)
         v_2 = self.att_read(q_1, seq_2, seq_len_2)
 
-        print("model_5")
+        # print("model_5")
         seq_1 = self.mid_layer(seq_1)
         seq_2 = self.mid_layer(seq_2)
 
-        print("model_6")
+        # print("model_6")
         h_2 = self.att_comp(v_1, seq_2, seq_len_2)
         h_1 = self.att_comp(v_2, seq_1, seq_len_1)
 
-        print("model_7")
+        # print("model_7")
         h_ab = torch.cat([h_1, h_2, h_1 - h_2], dim=-1)  # [bs, 3 * n_h]
         h_ba = torch.cat([h_2, h_1, h_2 - h_1], dim=-1)  # [bs, 3 * n_h]
         h_final = self.out_layer(h_ab) + self.out_layer(h_ba) / 2.
 
-        print("model_8")
+        # print("model_8")
         return self.final_layer(h_final).view(-1)  # [batch]
 
 
@@ -152,35 +152,33 @@ class BatchScaledDotProductAttention(nn.Module):
                 seq_length ( ... )
             note: dv == dk
         '''
-        print("Attn_1")
+        # print("Attn_1")
         h = self.q_layer(h).unsqueeze(-1)  # [ batch * h * 1 ]
         k = self.k_layer(seq)  # [ batch, length, h ]
         v = self.v_layer(seq)  # [ batch, length, h ]
 
-        print("Attn_2")
+        # print("Attn_2")
         scores = torch.matmul(k, h)  # [batch, length, 1]
-        print("scores", scores.size())
+        # print("scores", scores.size())
         scores = scores.squeeze(2)  # [ batch, length ]
-        print("scores", scores.size())
+        # print("scores", scores.size())
 
-        print("Attn_3")
+        # print("Attn_3")
         kv_mask = self.get_a_mask(seq_length)  # [ batch, length ]
-        print("kv_mask", kv_mask.size())
+        # print("kv_mask", kv_mask.size())
 
-        print("Attn_4")
+        # print("Attn_4")
         scores = scores.masked_fill(kv_mask == 0, -1e9)
 
-        print("Attn_5")
+        # print("Attn_5")
         weight = F.softmax(scores / self.temper, dim=-1).unsqueeze(1)
-        print("weight", weight.size())  # [batch, 1, length]
+        # print("weight", weight.size())  # [batch, 1, length]
         output = torch.matmul(weight, v)  # [batch, 1, length] -> [batch, 1, h]
-        print("output", output.size())  # [batch, 1, length]
+        # print("output", output.size())  # [batch, 1, length]
 
-        print("Attn_6")
+        # print("Attn_6")
         output = self.out_linear(output.squeeze(1))  # [batch, h]
         output = F.elu(output)
-
-        print("Attn_7")
         return output
 
     # Generate a boolean mask
