c85811bb21b2948eac6ae09543f707ceaddb963b
Hanjun Goo <goohanjun@gmail.com>
2020-05-13 17:59:16 +0900

    min max

--- model.py
+++ model.py
@@ -33,9 +33,9 @@ class ApproxEMD(nn.Module):
 
         # [batch, length, n_dim]
         mean_seq, max_seq, min_seq = 0., 0., 0.
-        q_1 = torch.cat([torch.max(seq_1, dim=1), torch.min(seq_1, dim=1), torch.sum(seq_1, dim=1) / seq_len_1.view(-1, 1)], dim=-1)
+        q_1 = torch.cat([torch.max(seq_1, dim=1)[0], torch.min(seq_1, dim=1)[0], torch.sum(seq_1, dim=1) / seq_len_1.view(-1, 1)], dim=-1)
         q_1 = self.query_act(self.query_layer(q_1))  # [batch, n_hidden]
-        q_2 = torch.cat([torch.max(seq_2, dim=1), torch.min(seq_2, dim=1), torch.sum(seq_2, dim=1) / seq_len_2.view(-1, 1)], dim=-1)
+        q_2 = torch.cat([torch.max(seq_2, dim=1)[0], torch.min(seq_2, dim=1)[0], torch.sum(seq_2, dim=1) / seq_len_2.view(-1, 1)], dim=-1)
         q_2 = self.query_act(self.query_layer(q_2))  # [batch, n_hidden]
 
         v_1 = self.att_read(q_2, seq_1, seq_len_1)
